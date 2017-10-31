02a36490dbf4b6f2c8300a998e273132f208c8cf
sarahjspd <sarahj.spd@gmail.com>
2017-10-31 18:06:21 +0800

    Devise added for members, demo purposes & admin page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   	resources :users, only: [:index]
     resources :jobs
 
- 	root to: "jobs#index"
+ 	root to: "admins#index"
+
+ 	resources :admins
+
 
 
 end
