7af09c2ecef807e50c4517f0e8571c64da8e1945
sarahjspd <sarahj.spd@gmail.com>
2017-11-07 14:41:59 +0800

    Added Category Bookmark Algo and updated jobs index list

--- app/services/generate_category_score_service.rb
+++ app/services/generate_category_score_service.rb
@@ -2,12 +2,16 @@ class GenerateCategoryScoreService
 
 	def call
 
-		job = Job.where(id: ApplicationScore.all.pluck(:originaljob_id).uniq).limit(100)
-	  #@job = Job.find(params[:id])
+		job = Job.where(id: ApplicationScore.all.pluck(:originaljob_id).uniq)
+    bookmark_job = Job.where(id: BookmarkScore.all.pluck(:originaljob_id).uniq)
 
-	  job.find_each(batch_size: 10) do |x|
+	  job.find_each do |x|
 	    generate_scores_for_one_job(x)
 	  end
+
+    bookmark_job.find_each do |y|
+      generate_scores_for_one_bookmark_job(y)
+    end
 	end
 
 	private
@@ -19,7 +23,7 @@ class GenerateCategoryScoreService
     array_of_jobcategoryid= Job.where(id: array_of_jobid).pluck(:JobCategoryId)
 
     array_of_jobid_and_categoryid = array_of_jobid.zip(array_of_jobcategoryid).flatten.compact
-    array_of_jobid_and_categoryid = array_of_jobid_and_categoryid .each_slice(2).to_a
+    array_of_jobid_and_categoryid = array_of_jobid_and_categoryid.each_slice(2).to_a
 
     array_of_jobid_and_categoryid.each do |t|
       if @jobcategory == t[1]
@@ -46,6 +50,41 @@ class GenerateCategoryScoreService
 
 
 	end
+
+  def generate_scores_for_one_bookmark_job(job)
+    @jobcategory = job.JobCategoryId
+
+    array_of_jobid = BookmarkScore.where(originaljob_id: job.id).pluck(:job_id)
+    array_of_jobcategoryid= Job.where(id: array_of_jobid).pluck(:JobCategoryId)
+
+    array_of_jobid_and_categoryid = array_of_jobid.zip(array_of_jobcategoryid).flatten.compact
+    array_of_jobid_and_categoryid = array_of_jobid_and_categoryid.each_slice(2).to_a
+
+    array_of_jobid_and_categoryid.each do |t|
+      if @jobcategory == t[1]
+        matched_count =1
+      else
+        matched_count =0
+      end
+
+      matched_category_score = matched_count*0.5
+      t.insert(2, matched_category_score)
+    end
+#[1, 5, 0.5]
+    array_of_jobid_and_categoryid.each do |catscore|
+
+
+        jobid = catscore[0]
+        score = catscore[2]
+        originaljob = job.id
+
+        unless score == 0.0
+        CategoryScore.create( originaljob_id: originaljob, job_id: jobid, cscore: score)
+        end
+    end
+
+
+  end
 
 
 
